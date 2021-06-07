09a718d44b9b75f727ff2e46c82c5ac96c0be45f
Ali-coder1996 <alisalamehhjouj@gmail.com>
2021-06-07 15:21:25 +0300

    add html css js in my code for the assigement

---
+++ js/app.js
@@ -0,0 +1,55 @@
+"use strict";
+let userName = prompt('please enter Name');
+alert(`welcome to my website ${userName} let is enjoy now you will be discover me by answer in yes or no or y or n okay `);
+
+let favoritePlace = prompt('my favorite place is turkey').toLowerCase();
+if (favoritePlace === 'yes' || favoritePlace === 'y'){
+    alert('great job')
+    // console.log('well done')
+}else if(favoritePlace === 'no' || favoritePlace === 'n'){
+    alert('wrong answer')
+}else {
+    alert('you should answer by yes or no or y or n')
+} 
+
+let favoriteSong = prompt('my favorite song is english').toLowerCase();
+if (favoriteSong === 'yes' || favoriteSong === 'y'){
+    alert('great job')
+    // console.log('well done')
+}else if(favoriteSong === 'no' || favoriteSong === 'n'){
+    alert('wrong answer')
+}else {
+    alert('you should answer by yes or no or y or n')
+}
+
+let favoriteDish = prompt('my favorite dish is pasta').toLowerCase();
+if (favoriteDish === 'yes' || favoriteDish === 'y'){
+    alert('great job')
+    // console.log('well done')
+}else if(favoriteDish === 'no' || favoriteDish === 'n'){
+    alert('wrong answer')
+}else {
+    alert('you should answer by yes or no or y or n')
+}
+
+let favoriteHair = prompt('I prefer short hair').toLowerCase();
+if (favoriteHair === 'yes' || favoriteHair === 'y'){
+    alert('wrong answer')
+}else if(favoriteHair === 'no' || favoriteHair === 'n'){
+    // console.log('well done')
+    alert('great job')
+}else {
+    alert('you should answer by yes or no or y or n')
+}
+
+let favoriteSport = prompt('I love to go gym').toLowerCase();
+if (favoriteSport === 'yes' || favoriteSport === 'y'){
+    alert('great job')
+    // console.log('well done')
+}else if(favoriteSport === 'no' || favoriteSport === 'n'){
+    alert('wrong answer')
+}else {
+    alert('you should answer by yes or no or y or n')
+}
+
+alert('just to know the rigth answer is yes,yes,yes,no.yes')
